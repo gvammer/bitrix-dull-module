18562f55151481902e83048bf14fad75e24a0cf3
gvammer <gvamm3r@gmail.com>
2019-08-01 15:06:33 +0400

    Быстрое добавление новых параметров модуля

--- options.php
+++ options.php
@@ -3,7 +3,6 @@
 use Bitrix\Main\Application;
 use Bitrix\Main\Config\Option;
 use Bitrix\Main\Localization\Loc;
-use Bitrix\Main\Text\String;
 
 defined('ADMIN_MODULE_NAME') or define('ADMIN_MODULE_NAME', 'bex.d7dull');
 
