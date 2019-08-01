5b74fa4b35378c63cb247f00c6b684e53210c2ae
gvammer <gvamm3r@gmail.com>
2019-08-01 14:23:31 +0400

    Быстрое добаыление новых параметров модуля

--- options.php
+++ options.php
@@ -15,16 +15,31 @@ $app = Application::getInstance();
 $context = $app->getContext();
 $request = $context->getRequest();
 
-Loc::loadMessages($context->getServer()->getDocumentRoot()."/bitrix/modules/main/options.php");
+Loc::loadMessages($context->getServer()->getDocumentRoot() . "/bitrix/modules/main/options.php");
 Loc::loadMessages(__FILE__);
 
-$tabControl = new CAdminTabControl("tabControl", array(
-    array(
-        "DIV" => "edit1",
-        "TAB" => Loc::getMessage("MAIN_TAB_SET"),
-        "TITLE" => Loc::getMessage("MAIN_TAB_TITLE_SET"),
-    ),
-));
+$arAllOptions = [
+    [
+        'max_image_size',
+        GetMessage('REFERENCES_MAX_IMAGE_SIZE'),
+        '40',
+        [
+            'text',
+            100
+        ]
+    ],
+];
+
+$aTabs = [
+    [
+        'DIV' => 'edit1',
+        'TAB' => Loc::getMessage("MAIN_TAB_SET"),
+        'TITLE' => Loc::getMessage("MAIN_TAB_TITLE_SET")
+    ],
+];
+
+$tabControl = new CAdminTabControl("tabControl", $aTabs, true, true);
+
 
 if ((!empty($save) || !empty($restore)) && $request->isPost() && check_bitrix_sessid()) {
     if (!empty($restore)) {
@@ -33,57 +48,100 @@ if ((!empty($save) || !empty($restore)) && $request->isPost() && check_bitrix_se
             "MESSAGE" => Loc::getMessage("REFERENCES_OPTIONS_RESTORED"),
             "TYPE" => "OK",
         ));
-    } elseif ($request->getPost('max_image_size') && ($request->getPost('max_image_size') > 0) && ($request->getPost('max_image_size') < 100000)) {
-        Option::set(
-            ADMIN_MODULE_NAME,
-            "max_image_size",
-            $request->getPost('max_image_size')
-        );
-        CAdminMessage::showMessage(array(
-            "MESSAGE" => Loc::getMessage("REFERENCES_OPTIONS_SAVED"),
-            "TYPE" => "OK",
-        ));
     } else {
-        CAdminMessage::showMessage(Loc::getMessage("REFERENCES_INVALID_VALUE"));
+
+        $bSaved = false;
+        foreach ($arAllOptions as $arOption) {
+
+            $optionName = $arOption[0];
+            $optionValue = $request->getPost($optionName);
+
+            if ($optionValue !== null) {
+                $fieldType = $arOption[3][0];
+
+                if ($fieldType == 'checkbox' && $optionValue != 'Y') $optionValue = 'N';
+
+                $bSaved = true;
+                Option::set(ADMIN_MODULE_NAME, $optionName, $optionValue);
+            }
+        }
+
+        if (!$bSaved) {
+            CAdminMessage::showMessage(Loc::getMessage("REFERENCES_INVALID_VALUE"));
+        } else {
+            CAdminMessage::showMessage(array(
+                "MESSAGE" => Loc::getMessage("REFERENCES_OPTIONS_SAVED"),
+                "TYPE" => "OK",
+            ));
+        }
+
     }
 }
 
 $tabControl->begin();
 ?>
 
-<form method="post" action="<?=sprintf('%s?mid=%s&lang=%s', $request->getRequestedPage(), urlencode($mid), LANGUAGE_ID)?>">
-    <?php
-    echo bitrix_sessid_post();
-    $tabControl->beginNextTab();
-    ?>
-    <tr>
-        <td width="40%">
-            <label for="max_image_size"><?=Loc::getMessage("REFERENCES_MAX_IMAGE_SIZE") ?>:</label>
-        <td width="60%">
-            <input type="text"
-                   size="50"
-                   maxlength="5"
-                   name="max_image_size"
-                   value="<?=String::htmlEncode(Option::get(ADMIN_MODULE_NAME, "max_image_size", 500));?>"
-                   />
-        </td>
-    </tr>
+<form method="post"
+      action="<?= sprintf('%s?mid=%s&lang=%s', $request->getRequestedPage(), urlencode($mid), LANGUAGE_ID) ?>">
 
-    <?php
+    <?
+    echo bitrix_sessid_post();
+
+    $tabControl->BeginNextTab();
+
+    foreach ($arAllOptions as $arOption) {
+
+        $optionName = $arOption[0];
+        $optionTitle = $arOption[1];
+        $optionDefaultValue = $arOption[2];
+        $optionValue = Option::get(ADMIN_MODULE_NAME, $optionName, $optionDefaultValue);
+        $fieldType = $arOption[3][0];
+        $fieldSizeAttr1 = $arOption[3][1];
+        $fieldSizeAttr2 = $arOption[3][2];
+        ?>
+      <tr>
+        <td width="40%" nowrap<? if ($fieldType == 'textarea') echo ' class="adm-detail-valign-top"'; ?>>
+          <span id="hint_<?= $optionName ?>"></span>
+          <label for="<?= $optionName ?>"><?= $optionTitle ?>:</label>
+        </td>
+        <td width="60%">
+            <?
+            switch ($fieldType) {
+                case 'checkbox':
+                    ?>
+                  <input type="hidden" name="<?= $optionName ?>" value="N">
+                  <input type="checkbox" id="<?= $optionName ?>" name="<?= $optionName ?>"
+                         value="Y"<? if ($optionValue == 'Y') echo ' checked'; ?>>
+                    <? break;
+                case 'text':
+                    ?>
+                  <input type="text" size="<?= $fieldSizeAttr1 ?>" maxlength="255" value="<?= $optionValue ?>"
+                         name="<?= $optionName ?>">
+                    <? break;
+                case 'textarea':
+                    ?>
+                  <textarea rows="<?= $fieldSizeAttr1 ?>" cols="<?= $fieldSizeAttr2 ?>"
+                            name="<?= $optionName ?>"><?= $optionValue ?></textarea>
+                    <? break;
+            } ?>
+        </td>
+      </tr>
+    <? }
+
     $tabControl->buttons();
     ?>
-    <input type="submit"
-           name="save"
-           value="<?=Loc::getMessage("MAIN_SAVE") ?>"
-           title="<?=Loc::getMessage("MAIN_OPT_SAVE_TITLE") ?>"
-           class="adm-btn-save"
-           />
-    <input type="submit"
-           name="restore"
-           title="<?=Loc::getMessage("MAIN_HINT_RESTORE_DEFAULTS") ?>"
-           onclick="return confirm('<?= AddSlashes(GetMessage("MAIN_HINT_RESTORE_DEFAULTS_WARNING")) ?>')"
-           value="<?=Loc::getMessage("MAIN_RESTORE_DEFAULTS") ?>"
-           />
+  <input type="submit"
+         name="save"
+         value="<?= Loc::getMessage("MAIN_SAVE") ?>"
+         title="<?= Loc::getMessage("MAIN_OPT_SAVE_TITLE") ?>"
+         class="adm-btn-save"
+  />
+  <input type="submit"
+         name="restore"
+         title="<?= Loc::getMessage("MAIN_HINT_RESTORE_DEFAULTS") ?>"
+         onclick="return confirm('<?= AddSlashes(GetMessage("MAIN_HINT_RESTORE_DEFAULTS_WARNING")) ?>')"
+         value="<?= Loc::getMessage("MAIN_RESTORE_DEFAULTS") ?>"
+  />
     <?php
     $tabControl->end();
     ?>
